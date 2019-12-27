8fea4ec9694b46d242616c9b6feff7b1e9b84bf9
whaleasauresrex <derpywhale09@gmail.con>
2019-12-26 21:56:13 -1000

    Minor changes to format and added a new logo to the screen.

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -4,16 +4,15 @@
         elevated
         class="bg-primary text-white"
         height-hint="98"
-
       >
-        <q-toolbar class="bg-website-mustard-yellow text-dark">
+        <q-toolbar class="bg-white text-dark">
           <q-toolbar-title >
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+            <q-avatar style="width:80px; height: 80px">
+              <img src="../statics/HonoluluBakingCircle.png">
             </q-avatar>
             Honolulu Baking Company
           </q-toolbar-title>
-          <q-tabs align="right" class="bg-website-mustard-yellow">
+          <q-tabs align="right">
             <q-route-tab to="/page1" label="About Us" />
             <q-route-tab to="/page2" label="Contacts" />
             <q-route-tab to="/page3" label="Clients" />
