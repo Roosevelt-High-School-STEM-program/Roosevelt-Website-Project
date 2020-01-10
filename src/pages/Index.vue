bdf39135fcee804101c808fcadda63e771d7a1ba
whaleasauresrex <derpywhale09@gmail.con>
2020-01-10 08:14:12 -1000

    Made small changes to code, but overall the first template to the website.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,41 @@
 <template>
-  <q-page
-  >
+  <q-page>
     <div
-      class="col-12 row justify-center"
-      style="background-image: url('../statics/pastry.jpg'); background-size: 100%;"
+      class="row"
+      style="
+      background-image: url('../statics/oatMealCake.jpg');
+      background-size: 100%;
+      font-family: Satisfy, cursive;
+      "
     >
-      <img class="col-3" src="../statics/HonoluluBakingCircle.png"/>
-      <h1>HONOLULU BAKING COMPANY</h1>
+      <div
+        class="col-12 row justify-center"
+        style="background-color: rgba(255,255,255,0.3)"
+      >
+        <img class="col-3" src="../statics/HonoluluBakingCircle.png"/>
+        <div class="col-12 row justify-center">
+          <h1 class="text-dark" >HONOLULU BAKING COMPANY</h1>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 row" style="background-color: #FFD046">
+      <div
+        class="col-6"
+        style="background-image: url('../statics/pastry.jpg'); background-size:100%"
+      >
+      </div>
+      <div
+        class="col-6"
+      >
+        <h1>
+          Incredibly Inspiring words Incredibly Inspiring words
+        </h1>
+        <p>
+          Incredibly Inspiring words Incredibly Inspiring words
+        </p>
+
+
+    </div>
     </div>
   </q-page>
 </template>
