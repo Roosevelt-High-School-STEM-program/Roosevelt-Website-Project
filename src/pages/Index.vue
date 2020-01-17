91c13c3e1341ca59ac82a3da373be7e98e9588bc
whaleasauresrex <derpywhale09@gmail.con>
2020-01-17 08:56:48 -1000

    Did small fixes to the columns and pinpointed the logic behind the background on the front page.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,10 @@
 <template>
   <q-page class="row">
-    <div class="col-lg-12 column" style="height:750px">
+    <div class="col-md-12 column" style="height:710px">
+      <!-- Note is that the box is not resizing because
+       the background is taking up 100% of the page -->
       <div
-        class="col-lg-10 row"
+        class="col-md-12 row"
         style="
         background-image: url('../statics/oatMealCake.jpg');
         background-size: 100%;
@@ -11,22 +13,22 @@
         "
       >
         <div
-          class="col-lg-12 row justify-center"
+          class="col-md-12 row justify-center"
           style="background-color: rgba(255,255,255,0.3)"
         >
-          <div class="col-lg-4 q-mt-lg">
+          <div class="col-md-4 q-mt-lg">
             <img class="col-4" src="../statics/HonoluluBakingCircle.png"/>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-lg-12 row" style="background-color: #FFD046">
+    <div class="col-md-12 row" style="background-color: #FFD046">
       <div
-        class="col-lg-6"
+        class="col-md-6"
         style="background-image: url('../statics/pastry.jpg'); background-size:100%"
       >
       </div>
-      <div class="col-lg-6">
+      <div class="col-md-6">
         <h1>
           Incredibly Inspiring words Incredibly Inspiring words
         </h1>
