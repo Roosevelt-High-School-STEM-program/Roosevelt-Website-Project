c4c87ab91ec6ec1affc27dbdac9e208d580f5852
whaleasauresrex <derpywhale09@gmail.con>
2020-02-03 10:35:31 -1000

    Currently making the page and adding in the finishing styling touchups for this branch.

---
+++ src/components/workersscreen/RegularWorkersScreen.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="row justify-center" style="height: 1870px; background-color: #F2C037;">
+    <h1
+      class="col-12 text-center text-bold q-mt-lg"
+      style="font-family: Satisfy, cursive;"
+    >
+      The Ohana
+    </h1>
+    <div class="col-md-1" />
+    <q-img class="col-md-10" :ratio="16/9" src="../../statics/pastry.jpg"/>
+    <div class="col-md-1" />
+    <div class="col-md-1" />
+    <div class="row col-md-10" style="font-size: 25px;font-family: Satisfy, cursive;">
+      <div
+        class="col-md-12 q-mt-md row items-center bg-website-white"
+        style="border-radius: 255px 0px 0px 250px"
+      >
+        <img
+          class="col-2"
+          style="width: 300px; height: 300px; border-radius: 50%;"
+          src="../../statics/pastry.jpg"
+        />
+        <p class="col-md-9 q-ml-md text-center">
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+        </p>
+      </div>
+
+      <div
+        class="col-md-12 q-mt-md row items-center bg-website-white"
+        style="border-radius: 200px 0px 0px 200px"
+      >
+        <img
+          class="col-2"
+          style="width: 300px; height: 300px; border-radius: 50%;"
+          src="../../statics/pastry.jpg"
+        />
+        <p class="col-md-9 q-ml-md" >
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+        </p>
+      </div>
+      <div
+        class="col-md-12 q-my-md row items-center bg-website-white"
+        style="border-radius: 200px 0px 0px 200px"
+      >
+        <img
+          class="col-2"
+          style="width: 300px; height: 300px; border-radius: 50%;"
+          src="../../statics/pastry.jpg"
+        />
+        <p class="col-md-9 q-ml-md" >
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+          This worker is incredible. I'm truly amazed by their performance.
+        </p>
+      </div>
+<!--      <img-->
+<!--        class="col-2 q-mb-md"-->
+<!--        style="width: 300px; height: 300px; border-radius: 50%;"-->
+<!--        src="../../statics/pastry.jpg"-->
+<!--      />-->
+<!--      <div class="col-md-9 q-ml-md q-mt-md row items-center" style="text-align:center;">-->
+<!--        <p class="col-md-12">-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--        </p>-->
+<!--      </div>-->
+<!--      <img-->
+<!--        class="col-2 q-mb-md"-->
+<!--        style="width: 300px; height: 300px;border-radius: 50%;"-->
+<!--        src="../../statics/pastry.jpg"-->
+<!--      />-->
+<!--      <div class="col-md-9 q-ml-md q-mt-md row items-center" style="text-align:center;">-->
+<!--        <p class="col-md-12">-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--          This worker is incredible. I'm truly amazed by their performance.-->
+<!--        </p>-->
+<!--      </div>-->
+    </div>
+    <div class="col-md-1" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegularWorkersScreen',
+};
+</script>
+
+<style scoped>
+
+</style>
+s
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,16 +2,17 @@
   <q-page>
     <regular-opening-screen></regular-opening-screen>
     <regular-about-us></regular-about-us>
-    <regular-opening-screen></regular-opening-screen>
+    <regular-workers-screen></regular-workers-screen>
   </q-page>
 </template>
 
 <script>
 import RegularOpeningScreen from '../components/openingscreen/RegularOpeningScreen';
 import RegularAboutUs from '../components/aboutus/RegularAboutUs';
+import RegularWorkersScreen from '../components/workersscreen/RegularWorkersScreen';
 
 export default {
   name: 'PageIndex',
-  components: { RegularOpeningScreen, RegularAboutUs },
+  components: { RegularWorkersScreen, RegularOpeningScreen, RegularAboutUs },
 };
 </script>
