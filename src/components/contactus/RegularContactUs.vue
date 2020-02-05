a606f770efdb3e160c73dc7fa1b0eb8227012d07
whaleasauresrex <derpywhale09@gmail.con>
2020-02-05 09:23:16 -1000

    Currently looking for a 2nd font to use on the page, and I am cconsidering using border to make a more seperate page, may throw in more colors to make the page look more diverse but so far the colors we have are not complimenting each other.

--- src/components/contactus/RegularContactUs.vue
+++ src/components/contactus/RegularContactUs.vue
@@ -1,27 +1,40 @@
 <template>
-  <div class="row text-white items-center" style="background-color: #424C55; height: 300px;">
-<!--    <div class="col-md-5">-->
+  <div
+    class="row"
+    style="border-top:2px solid black;"
+  >
+    <div
+      class="row col-md-12 text-white justify-center q-my-none bg-website-white"
+      style="height: 325px;"
+    >
+<!--    <div class="col-md-3">-->
 <!--      <q-img-->
 <!--        src="../../statics/HonoluluBakingCircle.png"-->
 <!--        style="max-width:200px"-->
 <!--        :ratio="1"-->
 <!--      />-->
-<!--    </div>-->
-    <div class="row col-md-12 text-center">
-      <div class="col-md-12">
-        <h1 class="q-mt-none">Contact Info</h1>
-      </div>
-      <div class="col-md-6">
-        <p>Address: 523 Ahui St, Honolulu, HI 96813</p>
-        <p>Email: badhakhds</p>
-        <p>Phone: (808) 596-8415</p>
-      </div>
-      <div class="col-md-6">
-        <p>Instagram: @originalhawaiioatcake</p>
-        <p>Facebook:</p>
+  <!--    </div>-->
+      <div class="row col-md-12 text-center text-dark">
+        <div class="col-md-12 q-my-none" style=" font-family: Satisfy, cursive;">
+          <h1 class="q-mb-sm">Contact Info</h1>
+        </div>
+        <div class="col-md-1"/>
+        <div class="col-md-5">
+          <p class="q-ma-sm text-weight-light">Address</p>
+          <p class="q-ma-sm text-weight-bold">523 Ahui St, Honolulu, HI 96813</p>
+  <!--        <p>Email: badhakhds</p>-->
+          <p class="q-ma-sm text-weight-light">Phone</p>
+          <p class="q-ma-sm text-weight-bold">(808) 596-8415</p>
+        </div>
+        <div class="col-md-5">
+          <p class="q-ma-sm text-weight-light">Instagram</p>
+          <p class="q-ma-sm text-weight-bold">@OriginalHawaiiOatCake</p>
+<!--          <a href="" class="q-ma-sm text-weight-bold">@OriginalHawaiiOatCake</a>-->
+          <p class="q-ma-sm text-weight-light">Facebook</p>
+        </div>
+        <div class="col-md-1"/>
       </div>
     </div>
-    <div class="col-md-12"></div>
   </div>
 </template>
 
