7a9ed9f4402bf09b40919361e79d638f0aa3c478
whaleasauresrex <derpywhale09@gmail.con>
2020-05-17 16:29:49 -1000

    The design of this is still a work in progress, but will soon come back after a proper design is made

--- src/components/workersscreen/simpletest.vue
+++ src/components/workersscreen/simpletest.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row justify-center bg-website-white" style="min-height: 1500px">
+  <div class="row justify-center bg-website-white" style="min-height: 1400px">
     <div class="row justify-center items-center col-md-12">
       <div class="col-md-3" />
       <div class="col-md-6">
@@ -20,10 +20,6 @@
       class="row col-md-10 justify-center"
       style="font-family: Alegreya Sans SC;"
     >
-      <!--      <div-->
-      <!--        class="col-md-9 q-mt-md row items-center bg-website-gray"-->
-      <!--        style="border-radius: 200px 0px 0px 200px; height: 300px;"-->
-      <!--      >-->
       <div>
         <div
           @mouseover="hover=true"
@@ -31,37 +27,8 @@
           class="col-2"
           :style="darkenImg"
         />
-        <!--        <div class="column col-md-7" style="height:300px">-->
-        <!--          <div class="col-md-3">-->
-        <!--            <h1-->
-        <!--              class="q-my-none q-ml-md text-weight-bolder text-center"-->
-        <!--              style="font-size: 32px"-->
-        <!--            >-->
-        <!--              Worker-->
-        <!--            </h1>-->
-        <!--          </div>-->
-        <!--          <div class="col-md-5">-->
-        <!--            <p-->
-        <!--              class="col-md-5 q-ml-md q-my-none q-ml-md"-->
-        <!--              style="font-size: 19px; font-family: Inter;"-->
-        <!--            >-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!--   This worker is incredible. I'm truly amazed by their performance. LIke truly wow-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!--            </p>-->
-        <!--          </div >-->
-        <!--        </div>-->
       </div>
       <div class="col-1"/>
-      <!--      <div class="column items-center">-->
-      <!--        <div class="col-md-1 q-mt-md top-triangle"/>-->
-      <!--        <div class="col-md-1 bottom-triangle" />-->
-      <!--      </div>-->
-      <!--      <div-->
-      <!--        class="col-md-9 q-mt-md row items-center bg-website-gray"-->
-      <!--        style="border-radius: 200px 0px 0px 200px; height: 300px"-->
-      <!--      >-->
       <div>
         <q-img
           class="col-2"
@@ -71,69 +38,15 @@
           border-radius: 50%; border: solid 3px black; background-color: rgba(0, 0, 0, 0.6)"
           src="../../statics/people-photos/worker-2.jpeg"
         />
-        <!--        <div class="column col-md-7" style="height:300px">-->
-        <!--          <div class="col-md-3">-->
-        <!--            <h1-->
-        <!--              class="q-my-none q-ml-md text-weight-bolder text-center"-->
-        <!--              style="font-size: 32px"-->
-        <!--            >-->
-        <!--              Worker-->
-        <!--            </h1>-->
-        <!--          </div>-->
-        <!--          <div class="col-md-5">-->
-        <!--            <p-->
-        <!--              class="col-md-5 q-ml-md q-my-none q-ml-md"-->
-        <!--              style="font-size: 19px; font-family: Inter;"-->
-        <!--            >-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!-- This worker is incredible. I'm truly amazed by their performance. LIke truly wow-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!--            </p>-->
-        <!--          </div >-->
-        <!--        </div>-->
       </div>
       <div class="col-1"/>
-      <!--      <div class="column items-center">-->
-      <!--        <div class="col-md-1 q-mt-md top-triangle"/>-->
-      <!--        <div class="col-md-1 bottom-triangle" />-->
-      <!--      </div>-->
-      <!--      <div-->
-      <!--        class="col-md-9 q-my-md row items-center bg-website-gray"-->
-      <!--        style="border-radius: 200px 0px 0px 200px; height: 300px;"-->
-      <!--      >-->
       <div>
         <q-img
           class="col-2"
           style="width: 300px; height: 300px; border-radius: 50%; border:solid 3px black"
           src="../../statics/people-photos/worker-3.jpeg"
         />
-        <!--        <div class="column col-md-7" style="height:300px">-->
-        <!--          <div class="col-md-3">-->
-        <!--            <h1-->
-        <!--              class="q-my-none q-ml-md text-weight-bolder text-center"-->
-        <!--              style="font-size: 32px"-->
-        <!--            >-->
-        <!--              Worker-->
-        <!--            </h1>-->
-        <!--          </div>-->
-        <!--          <div class="col-md-5">-->
-        <!--            <p-->
-        <!--              class="col-md-5 q-ml-md q-my-none q-ml-md"-->
-        <!--              style="font-size: 19px; font-family: Inter;"-->
-        <!--            >-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!-- This worker is incredible. I'm truly amazed by their performance. LIke truly wow-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!--              This worker is incredible. I'm truly amazed by their performance.-->
-        <!--            </p>-->
-        <!--          </div >-->
-        <!--        </div>-->
       </div>
-      <!--      <div class="column items-center">-->
-      <!--        <div class="col-md-1 q-mt-md top-triangle" />-->
-      <!--        <div class="col-md-1 bottom-triangle" />-->
-      <!--      </div>-->
     </div>
 
   </div>
